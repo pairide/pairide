6b986d5017356369113070ae5a72f9225fe5e821
Warren Marivel <ufowam@gmail.com>
2013-01-14 22:38:46 -0500

    started registration page

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
+app.get('/register', routes.register);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'sadihsaifh', current : 'Home' });
 };
+
+exports.register = function(req, res){
+	res.render('register', { title: 'Register', current : "None"});
+}
