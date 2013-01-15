0541bd0da1a9f707731b13fc81a5696100002a49
Arod <alex.rodrigues@mail.utoronto.ca>
2013-01-14 22:07:16 -0500

    Added about pages & horizontal breaks to the template

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,10 @@
 exports.index = function(req, res){
   res.render('index', { title: 'sadihsaifh', current : 'Home' });
 };
+
+/*
+ * Route the user and render the about page.
+ */
+exports.about = function(req, res){
+  res.render('about', { title: 'sadihsaifh', current : 'Home' });
+};
