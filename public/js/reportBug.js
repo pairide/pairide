0b9f8ed0c6804598d3216e2c1c751fc3796884e4
Arod <alex.rodrigues@mail.utoronto.ca>
2013-01-16 18:02:39 -0500

    Contact file was not being tracked

---
+++ public/js/reportBug.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	$('#reportBugForm').validate({
+		errorElement: "span",
+		rules: {
+			bugReportBox : {
+				required: true,
+				minlength: 10,
+				maxlength: 1024
+			}
+		}
+	});
+});
