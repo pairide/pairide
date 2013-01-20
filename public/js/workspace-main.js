d3b7bcda1aff95691f341fc655d65f256ca70b89
Warren Marivel <ufowam@gmail.com>
2013-01-20 17:20:26 -0500

    created workspace

--- public/js/workspace-main.js
+++ public/js/workspace-main.js
@@ -1,5 +1,25 @@
+language = "python";
+
 $(document).ready(function(){
-
-
-
+	setUpEditor();
 });
+
+
+/* Set up editor space with syntax highlight,
+*  auto-indent and bracket matching
+*/
+function setUpEditor() {
+
+	editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+		mode : {
+			name : language
+		},
+		lineNumbers : true,
+		indentUnit : 4,
+		tabMode : "shift",
+		theme : "monokai",
+		matchBrackets : true
+		//onChange : sendCode
+	});
+
+}
