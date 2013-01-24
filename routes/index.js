787e8b30c7da9a21378bb90ae0c57e72d24865f6
Arod <alex.rodrigues@mail.utoronto.ca>
2013-01-23 19:57:05 -0500

    client file browser is now successfully connected to the server side workspace using ajax post requests. Files still need to be given icons determined by their extension.

--- routes/index.js
+++ routes/index.js
@@ -46,24 +46,52 @@ exports.workspace = function(req, res){
  * Handle the ajax POST request for the file browser.
  */
 exports.fileConnector = function(req, res){
-  res.render("filedemo"); //currently always rendering fake files
+  //res.render("filedemo"); //currently always rendering fake files
   console.log("Handling post to file manager")
 
   fs = require('fs');
   var directory = process.cwd() + "/users"; 
   var username = "alex"; //this needs to be based on the current user
-  var path = directory + "/" + username;
-  console.log(path);
-  fs.readdir(path, function (err, files) {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    var html = "<ul class=\"jqueryFileTree\">";
+  var path = directory + "/" + username + req.body.dir;
+  console.log("Path: " + path);
+  try {
+    //raises an error if the path does not exist
+    stats = fs.lstatSync(path);
 
-    html += "</ul>"
-    console.log(html);
-  });
+    if (stats.isDirectory()) {
+        fs.readdir(path, function (err, files) {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          var html = "<ul style=\"display: none;\" class=\"jqueryFileTree\">";
+          for (var i=0; i < files.length; i++){
+            try{
+              var filePath = path + files[i];
+              var fileStats = fs.lstatSync(filePath);
+              //check if file is a nested directory
+              if (fileStats.isDirectory()){
+              //might want to decode the html entities for filename
+                html +=  "<li class=\"directory collapsed\"><a href=\"#\" rel=\"" 
+                + req.body.dir + files[i] + "/\">" + files[i] + "</a></li>";
+              }
+              else if (fileStats.isFile()){
+                html +=  "<li class=\"directory collapsed\"><a href=\"#\" rel=\"" 
+                + req.body.dir + files[i] + "/\">" + files[i] + "</a></li>";
+              }
+            }catch(e){
+              console.log(e);
+            }
+          }
+        html += "</ul>";
+        res.send(html);
+      });
+    }
+  }
+  catch (e) {
+    console.log("Path to user workspace does not exist");
+  }
+
 }
 
 //testing page for the database -- to be removed later
