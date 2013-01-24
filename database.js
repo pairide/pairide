224e3bcdbd372d7a68b7f02637da3b698d97589f
Warren Marivel <ufowam@gmail.com>
2013-01-24 01:15:20 -0500

    started working on random sessions

--- database.js
+++ database.js
@@ -1,40 +1,4 @@
 /* All functions related to db operations 
 * will be here
 */
-
-var mongo = require('mongoskin');
-var conn = mongo.db('mongodb://localhost:27017/test');
-var users = conn.collection('users');
-
-//Get a list of all users
-exports.getUser = function(user){
-	users.findOne({ username: user}, function(err, item){
-		if(!item){
-			console.log(user + " does not exist");
-		}
-		else{
-			console.log(item.username);
-		}
-	});
-}
-
-exports.insertUser = function(user){
-	users.insert([user], function(err, result){
-		if(err){
-			return false;
-		}
-		else{
-			console.log(result);
-			return true;
-		}
-	});
-};
-
-/* User Schema
-*  {username: required, unique, length=16,
-	password_hash: length=32,
-	salt: ,
-	email: required, unique,
-	validate: bool	
-	}
-*/
+var mongo = require('mongoose');
