73eb57a80d9f1d6ca4957b354bb09d605ea4bbe2
Arod <alex.rodrigues@mail.utoronto.ca>
2013-01-30 16:07:15 -0500

    Editor now supports drag and drop

--- public/js/workspace-main.js
+++ public/js/workspace-main.js
@@ -31,52 +31,61 @@ function setUpEditor() {
     editor.setTheme("ace/theme/monokai");
     editor.getSession().setMode("ace/mode/javascript");
     document.getElementById('code').style.fontSize='14px';
-    editor.setValue("hello");
 }
 
 
-/* Handle when a file is dragged onto the editor. */
+/* Handle when a file is dragged onto the editor using some html5.
+ * This may not work for all browsers.
+ */
 function handleDragOn(evt) {
 
+    // Check for the various File API support.
+    if (!(window.File && window.FileReader && window.FileList)) {
+        //may want to supress this later
+      alert('Your browser does not fully support drag and drop.');
+      return;
+    } 
     evt.stopPropagation();
     evt.preventDefault();
-    alert(evt.dataTransfer);
+    evt.dataTransfer = evt.originalEvent.dataTransfer;
+    if (!evt.dataTransfer) return;
     var files = evt.dataTransfer.files;
-    var output = [];
-    for (var i = 0, f; f = files[i]; i++) {
-        output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ', f.size, ' bytes, last modified: ', f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a', '</li>');
+    
+    //only read the first file on the filelist
+    //although the client may be dragging more then one at a time
+    //the others are ignored; this is a design choice.
+    if (files && files[0]){
+        var f = files[0];
+        editor.setValue(""); //clear it
+        handleFileSelect(f);
+        editor.setValue(uploadClipBoard);
     }
 
-    editor.setValue("");
-    editor.setValue('<ul>' + output.join('') + '</ul>');
+    
 }
 
 /* Handle when a file is dragged over the editor. */
 function handleDragOver(evt) {
-    alert(evt);
+    evt.dataTransfer = evt.originalEvent.dataTransfer;
+    if (!evt.dataTransfer) {
+        return;
+    }
     evt.stopPropagation();
     evt.preventDefault();
     evt.dataTransfer.dropEffect = 'copy';
 }
 
 /**
-* Read a local file from the evt.
-* @param {Object} evt
-*/
-function handleFileSelect(evt) {
-
-    var files = evt.target.files;
-    // FileList object
-    for (var i = 0, f; f = files[i]; i++) {
-
-        var reader = new FileReader();
-
-        // Closure to capture the file information.
-        reader.onload = (function(theFile) {
-            return function(e) {
-                uploadClipBoard = e.target.result;
-            };
-        })(f);
-        reader.readAsText(f);
-    }
+ * Reads a file on the clients computer and sets the upload clip board
+ * to the files contents.
+ */
+function handleFileSelect(f) {
+    var reader = new FileReader();
+    // Closure to capture the file information.
+    reader.onload = (function(theFile) {
+        return function(e) {
+            uploadClipBoard = e.target.result;
+        };
+    })(f);
+    reader.readAsText(f);
 }
