2583a8710b8b0ac48e0aa3bb826dde1b8e2d76c4
Arod <alex.rodrigues@mail.utoronto.ca>
2013-01-30 23:46:59 -0500

    Tracking

---
+++ public/js/basesockets.js
@@ -0,0 +1,36 @@
+var host = window.location.hostname;
+var port = 8000;
+var url = "http://"+host+":"+port;
+var isDriver; 
+
+function load(socket){
+	socket.on('is_driver', function(data){
+		isDriver = data.driver;
+		alert(isDriver);
+	});
+}
+
+/*Set up a socket connection
+* for the user */
+function connect(){
+	return io.connect(url);
+}
+
+/* Get the room's id */
+function roomID(type){
+	var matchRoomRequest;
+
+	//urls for express sessions and normal sessions 
+	//are not the same 
+	if(type=="workspace"){
+		matchRoomRequest = /.*\/workspace\/(.{32})/;
+	}
+	else{
+		matchRoomRequest = /.*\/express\/(.{32})/;
+	}
+	var match = matchRoomRequest.exec(document.URL);
+
+	if(match){
+		return match[1];
+	}
+}
--- public/js/workspace-main.js
+++ public/js/workspace-main.js
@@ -6,8 +6,6 @@ $(document).ready(function(){
 	requestWorkspace();
     $('#code').on('dragover', handleDragOver);
     $('#code').on('drop', handleDragOn);
-
-
 });
 
 /*
