dfb4be8d75fc2212fcc44ec88ae2c1adf3ce079f
Arod <alex.rodrigues@mail.utoronto.ca>
2013-03-01 01:15:04 -0500

    Save and switch between files

--- sockets/index.js
+++ sockets/index.js
@@ -18,6 +18,10 @@ var roomAdmins = {};
 //stored as an absolute path.
 var roomFile = {};
 
+//Maintains a mapping of socket id to socket object.
+//roomSockets[room][id] -> socket object
+var roomSockets = {};
+
 var workspace =  require('./socket_workspace');
 
 exports.roomAdmins = roomAdmins;
@@ -30,7 +34,8 @@ exports.communicate = function(io){
       //Handles a socket disconnecting.
       socket.on("disconnect", function(){
         console.log("disconnect detected");
-        workspace.disconnect(io, socket, roomDrivers, roomUsers, roomAdmins);
+        workspace.disconnect(io, socket, roomDrivers, roomUsers, 
+          roomAdmins, roomFile, roomSockets);
       });
 
       //Handle request to get members in a room
@@ -41,30 +46,34 @@ exports.communicate = function(io){
 
       //socket handler for users requesting to join a room
       socket.on('join_room', function(data) {
-          workspace.join(socket, data, roomDrivers, roomUsers, roomAdmins, roomFile);
+          workspace.join(socket, data, roomDrivers, roomUsers, roomAdmins,
+           roomFile, roomSockets);
           io.sockets.in(socket.store.data.room).emit('new_user', data);
       });
 
       socket.on("context_menu_clicked", function(data){
-        workspace.menuClicked(socket, data, roomDrivers, roomUsers, roomAdmins);
+        workspace.menuClicked(socket, data, roomDrivers, roomUsers,
+         roomAdmins);
       });
 
       //relay the message that the editor has changed
       socket.on("editor_changed", function(data){
           var room = socket.store.data.room;
           if (roomDrivers[room] == socket.id){
-            io.sockets.in(socket.store.data.room).emit('editor_update', data);
+            io.sockets.in(socket.store.data.room).emit('editor_update',
+             data);
           }
       });
 
       //Emit new message to all members in the room
       socket.on('send_message', function(data){
-        io.sockets.in(socket.store.data.room).emit('new_message', data);
+        io.sockets.in(socket.store.data.room).emit('new_message',
+         data);
       });
 
       socket.on("post_selection", function(data){
-        io.sockets.in(socket.store.data.room).emit('get_selection', data);
-        //console.log("SELECTION-> " + data.user);
+        io.sockets.in(socket.store.data.room).emit('get_selection',
+         data);
       });
 
       socket.on("post_annotation", function(data){
@@ -99,7 +108,7 @@ exports.communicate = function(io){
       socket.on('get_file', function(data){
         console.log("user " + data.user + " requested file " + data.fileName);
         workspace.changeFile(socket, data, roomDrivers, roomUsers, 
-          roomAdmins, roomFile, io);
+          roomAdmins, roomFile, roomSockets, io);
       })
   });
 };
